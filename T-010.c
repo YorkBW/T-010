bee381fdf40fdf9e944c2a31e4754a18f5c4b12a
Brent W. York <brent@artsimmagic.com>
2014-10-30 21:58:05 -0400

    Interim version
    Last check-in before removing explicit support for MPL115A2 (replaced
    with MPL3115A2).

--- T-010.c
+++ T-010.c
@@ -7,6 +7,17 @@
  *  Author: Brent W. York
  */ 
 
+/*
+Intended for use on an ATTiny4313
+Fuses must be set as follows:
+	lfuse: 0xef (stock is 0x64)
+	hfuse: 0xdf (same as stock)
+	efuse: 0xff (same as stock)
+	lock:  0x3f (same as stock)
+
+Do this by executing (on a factory-fresh IC):
+	avrdude -c usbtiny -p attiny4313 -U lfuse:w:0xEF:m
+*/
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -24,7 +35,7 @@
 //-------------------------
 	// Define USING_MPL3115 if the newer chip is present;
 	//  else MPL115 is assumed
-//#define USING_MPL3115	1
+#define USING_MPL3115	1
 	// Data loop times in milliseconds
 #define DATA_TICK              50
 #define HEARTBEAT_TICK       2000
@@ -35,14 +46,17 @@
 //-----------------------------
 //  I2C INTERFACE DEFINITIONS
 //-----------------------------
-const uint8_t MPL115A2_DEVICE        = 0xC0;
-const uint8_t MPL115A2_READPRESMSB   = 0x00;
-const uint8_t MPL115A2_READPRESLSB   = 0x01;
-const uint8_t MPL115A2_READTEMPMSB   = 0x02;
-const uint8_t MPL115A2_READTEMPLSB   = 0x03;
-const uint8_t MPL115A2_READCOEFFIRST = 0x04;
-const uint8_t MPL115A2_READCOEFLAST  = 0x0B;
-const uint8_t MPL115A2_STARTCONV     = 0x12;
+const uint8_t MPLx115A2_DEVICE        = 0xC0;
+const uint8_t MPL115A2_READPRESMSB    = 0x00;
+const uint8_t MPL115A2_READPRESLSB    = 0x01;
+const uint8_t MPL115A2_READTEMPMSB    = 0x02;
+const uint8_t MPL115A2_READTEMPLSB    = 0x03;
+const uint8_t MPL115A2_READCOEFFIRST  = 0x04;
+const uint8_t MPL115A2_READCOEFLAST   = 0x0B;
+const uint8_t MPL115A2_STARTCONV      = 0x12;
+
+const uint8_t MPU6050_DEVICE		  = 0xD0;
+const uint8_t MPU6050_WHOAMI		  = 0x75;
 
 
 //-----------------------------
@@ -63,9 +77,10 @@ const uint8_t FLASH_JEDEC_ID_CMD     = 0x9f;
 //  USB INTERFACE DEFINITIONS
 //-----------------------------
 	// USB messages from host
-#define USB_STATUS    0
-#define USB_DATA_DUMP 1
-#define USB_PRESSURE  2
+#define USB_STATUS			0
+#define USB_DATA_DUMP		1
+#define USB_ALTITUDE		2
+#define USB_TEMPERATURE		3
 	// USB register configuration
 	// Configures USI to 3-wire master mode with overflow interrupt
 #define myUSICR  (_BV(USIWM0) | _BV(USICS1) | _BV(USICLK))
@@ -83,12 +98,15 @@ const uint8_t FLASH_JEDEC_ID_CMD     = 0x9f;
 static uchar dataBuf[DATA_BUF_SZ];
 
 	// Status byte
-#define DEV_GOOD_MPL115A2		0
+#define DEV_GOOD_MPLX115		0
 #define DEV_GOOD_FLASH			1
+#define DEV_GOOD_MPU6050		2
 uchar deviceStatus;
 
 	// MPL115A2 coefficients
+#if !defined(USING_MPL3115)
 int16_t a0, b1, b2, c12;
+#endif
 
 	// Flash memory
 uint16_t flashSize;			// size in 256-byte pages
@@ -101,18 +119,85 @@ uint8_t flashByteCtr;		// current write byte counter within page
 //  I2C INTERFACE FUNCTIONS
 //---------------------------
 
-uint8_t initMPL115A2(void)
-// Initialize the MPL115A2 pressure sensor, return 0 if bad comms
+uint8_t writeI2CRegister(uint8_t device, uint8_t reg, uint8_t val)
 {
-	uchar coef;
+	if (i2c_start(device | I2C_WRITE) != 0) return 0;
+	i2c_write(reg);
+	i2c_write(val);
+	i2c_stop();
+	return 1;
+}
+
+
+uint8_t readI2CRegister(uint8_t device, uint8_t reg)
+{
+	uint8_t val;
 	
+	i2c_start_wait(device | I2C_WRITE);
+	i2c_write(reg);
+	i2c_rep_start(device | I2C_READ);
+	val = i2c_readNak();
+	i2c_stop();
+	
+	return val;
+}
+
+
+uint8_t writeMPLx115Register(uint8_t reg, uint8_t val)
+{
+	if (i2c_start(MPLx115A2_DEVICE | I2C_WRITE) != 0) return 0;
+	i2c_write(reg);
+	i2c_write(val);
+	i2c_stop();
+	return 1;
+}
+
+
+uint8_t readMPLx115Register(uint8_t reg)
+{
+	uint8_t val;
+	
+	i2c_start_wait(MPLx115A2_DEVICE | I2C_WRITE);
+	i2c_write(reg);
+	i2c_rep_start(MPLx115A2_DEVICE | I2C_READ);
+	//i2c_readAck(); // dummy read
+	val = i2c_readNak();
+	i2c_stop();
+	
+	return val;
+}
+
+
+uint8_t initMPLx115(void)
+// Initialize the MPLx115A2 pressure sensor / altimeter, return 0 if bad comms
+{
+#ifdef USING_MPL3115
+
+// Set to altimeter with an OSR = 128
+	if (!writeMPLx115Register(0x26, 0xB8)) return 0;
+
+// Set user offset altitude (TODO: make a real offset, not a test!)
+	//if (!writeMPLx115Register(0x2D, 35)) return 0;
+
+// Enable data flags in PT_DATA_CFG
+	if (!writeMPLx115Register(0x13, 0x07)) return 0;
+
+// Start data collection
+	if (!writeMPLx115Register(0x26, 0xB9)) return 0;
+	
+	// TEMP device check!
+	dataBuf[1] = readMPLx115Register(0x0C);
+	dataBuf[2] = 0xFF;
+
+#else
+
 	// Init comms
-	if (i2c_start(MPL115A2_DEVICE | I2C_WRITE) != 0) return 0;
+	if (i2c_start(MPLx115A2_DEVICE | I2C_WRITE) != 0) return 0;
 	
 	// Read coefficients
 	i2c_write(MPL115A2_READCOEFFIRST);
-	i2c_rep_start(MPL115A2_DEVICE | I2C_READ);
-	for (coef=MPL115A2_READCOEFFIRST; coef<=MPL115A2_READCOEFLAST; coef++) {
+	i2c_rep_start(MPLx115A2_DEVICE | I2C_READ);
+	for (uchar coef=MPL115A2_READCOEFFIRST; coef<=MPL115A2_READCOEFLAST; coef++) {
 		dataBuf[coef - MPL115A2_READCOEFFIRST] = i2c_readAck();
 	}
 	
@@ -123,29 +208,81 @@ uint8_t initMPL115A2(void)
 	b2  = (int16_t)((dataBuf[4] << 8) | dataBuf[5]);	// 14 fractional bits
 	c12 = (int16_t)((dataBuf[6] << 8) | dataBuf[7]);    // 9 dec pt zero pad
 
+#endif
+
 	return 1;	// device good
 }
 
 
-void startMPL115A2Read(void)
-// Start ADC conversion on MPL115A2 (needs up to 3ms to complete)
+void startMPLx115Read(void)
 {
-	i2c_start_wait(MPL115A2_DEVICE | I2C_WRITE);
+#ifdef USING_MPL3115
+// Check for data ready
+// TODO: Add a timeout!
+	uint8_t status;
+	do {
+		status = readMPLx115Register(0x00);
+	} while ((status & 0x08) == 0);
+#else
+// Start ADC conversion on MPL115A2 (needs up to 3ms to complete)
+	i2c_start_wait(MPLx115A2_DEVICE | I2C_WRITE);
 	i2c_write(MPL115A2_STARTCONV);
 	i2c_write(0);
 	i2c_stop();
+#endif
 }
 
 
-int16_t readMPL115A2Pressure(void)
-// Read data from MPL115A2 and return pressure in kPa (with 4-bit fractional part)
+int32_t readMPLx115Altitude(void)
+// Read data from MPLx115A2 and return altitude with 5-bit fractional part... (was pressure in kPa (with 4-bit fractional part))
 {
+#ifdef USING_MPL3115
+	uint8_t OUT_P_MSB, OUT_P_CSB, OUT_P_LSB;
+	int16_t res;
+	
+	// Read OUT_P and OUT_T
+	OUT_P_MSB = readMPLx115Register(0x01);
+	OUT_P_CSB = readMPLx115Register(0x02);
+	OUT_P_LSB = readMPLx115Register(0x03);
+	//OUT_T_MSB = readMPLx115Register(0x04);
+	//OUT_T_LSB = readMPLx115Register(0x05);
+	dataBuf[3] = OUT_P_MSB;
+	dataBuf[4] = OUT_P_CSB;
+	dataBuf[5] = OUT_P_LSB;
+	
+	dataBuf[10] = readMPLx115Register(0x04);
+	dataBuf[11] = readMPLx115Register(0x05);
+	
+	dataBuf[7] = OUT_P_MSB;
+	dataBuf[8] = OUT_P_CSB;
+	dataBuf[9] = OUT_P_LSB;
+	if (OUT_P_MSB & 0x80) {
+		dataBuf[6] = 1;
+		if (dataBuf[9]-- == 0) {
+			if (dataBuf[8]-- == 0) {
+				dataBuf[7]--;
+			}
+		}
+		dataBuf[7] = ~dataBuf[7];
+		dataBuf[8] = ~dataBuf[8];
+		dataBuf[9] = ~dataBuf[9];
+	}
+	else {
+		dataBuf[6] = 0;
+	}
+	
+	
+	res = (((uint16_t)OUT_P_MSB) << 13) & (((uint16_t)OUT_P_CSB) << 5) & (OUT_P_LSB >> 3);	// TODO: Check for negatives...this is 2s-complement data
+	return res;
+	
+#else
+
 	uint16_t Padc, Tadc;
 	int32_t c12x2, a1, a1x1, y1, a2x2, PComp;
 	
-	i2c_start_wait(MPL115A2_DEVICE | I2C_WRITE);
+	i2c_start_wait(MPLx115A2_DEVICE | I2C_WRITE);
 	i2c_write(0);
-	i2c_rep_start(MPL115A2_DEVICE | I2C_READ);
+	i2c_rep_start(MPLx115A2_DEVICE | I2C_READ);
 	Padc = (i2c_readAck() << 2) + (i2c_readAck() >> 6);
 	Tadc = (i2c_readAck() << 2) + (i2c_readNak() >> 6);
 	i2c_stop();
@@ -157,7 +294,86 @@ int16_t readMPL115A2Pressure(void)
 	a2x2  = (((int32_t)b2) * Tadc) >> 1;	// a2x2  = b2  * Tadc
 	PComp = (y1 + a2x2) >> 9;				// PComp = y1  + a2x2
 
-	return (int16_t)((((int32_t)PComp) * 1041) >> 14) + 800;
+	return (int16_t)((((int32_t)PComp) * 1041) >> 14) + 800;		// TODO: Convert to altitude!
+
+#endif
+}
+
+
+int16_t readMPLx115Temperature(void)
+// Read data from MPLx115A2 and return temperature in DegC
+{
+#ifdef USING_MPL3115
+
+	uint8_t OUT_T_MSB, OUT_T_LSB;
+	int16_t res;
+	
+	// Read OUT_P and OUT_T
+	OUT_T_MSB = readMPLx115Register(0x04);
+	OUT_T_LSB = readMPLx115Register(0x05);
+	dataBuf[3] = OUT_P_MSB;
+	dataBuf[4] = OUT_P_CSB;
+	dataBuf[5] = OUT_P_LSB;
+	
+	dataBuf[10] = readMPLx115Register(0x04);
+	dataBuf[11] = readMPLx115Register(0x05);
+	
+	dataBuf[7] = OUT_P_MSB;
+	dataBuf[8] = OUT_P_CSB;
+	dataBuf[9] = OUT_P_LSB;
+	if (OUT_P_MSB & 0x80) {
+		dataBuf[6] = 1;
+		if (dataBuf[9]-- == 0) {
+			if (dataBuf[8]-- == 0) {
+				dataBuf[7]--;
+			}
+		}
+		dataBuf[7] = ~dataBuf[7];
+		dataBuf[8] = ~dataBuf[8];
+		dataBuf[9] = ~dataBuf[9];
+	}
+	else {
+		dataBuf[6] = 0;
+	}
+	
+	
+	res = (((uint16_t)OUT_P_MSB) << 13) & (((uint16_t)OUT_P_CSB) << 5) & (OUT_P_LSB >> 3);	// TODO: Check for negatives...this is 2s-complement data
+	return res;
+	
+#else
+
+	uint16_t Padc, Tadc;
+	int32_t c12x2, a1, a1x1, y1, a2x2, PComp;
+	
+	i2c_start_wait(MPLx115A2_DEVICE | I2C_WRITE);
+	i2c_write(0);
+	i2c_rep_start(MPLx115A2_DEVICE | I2C_READ);
+	Padc = (i2c_readAck() << 2) + (i2c_readAck() >> 6);
+	Tadc = (i2c_readAck() << 2) + (i2c_readNak() >> 6);
+	i2c_stop();
+
+	c12x2 = (((int32_t)c12) * Tadc) >> 11;	// c12x2 = c12 * Tadc
+	a1    = (int32_t)b1 + c12x2;			// a1    = b1  + c12x2
+	a1x1  = a1 * Padc;						// a1x1  = a1  * Padc
+	y1    = (((int32_t)a0) << 10) + a1x1;	// y1    = a0  + a1x1
+	a2x2  = (((int32_t)b2) * Tadc) >> 1;	// a2x2  = b2  * Tadc
+	PComp = (y1 + a2x2) >> 9;				// PComp = y1  + a2x2
+
+	return (int16_t)((((int32_t)PComp) * 1041) >> 14) + 800;		// TODO: Convert to altitude!
+
+#endif
+}
+
+
+uint8_t initMPU6050(void)
+// Initialize the MPU-6050 accelerometer, return 0 if bad comms
+{
+	uint8_t c;
+	
+	c = readI2CRegister(MPU6050_DEVICE, MPU6050_WHOAMI);
+	if (c != 0x68) return 0;
+	
+	return 1;
 }
 
 
@@ -292,7 +508,7 @@ USB_PUBLIC uchar usbFunctionSetup(uchar data[8])
 {
 	//int temp;
 	//uint16_t utemp;
-	int16_t pressure;
+	//int16_t altitude;
 	
 	usbRequest_t *rq = (usbRequest_t *)data;
 	
@@ -307,12 +523,25 @@ USB_PUBLIC uchar usbFunctionSetup(uchar data[8])
 			usbMsgPtr = (usbMsgPtr_t)dataBuf;
 			return DATA_BUF_SZ;
 
-		case USB_PRESSURE:
-			startMPL115A2Read();
+		case USB_ALTITUDE:
+			startMPLx115Read();
+			#if !defined(USING_MPL3115)
 			_delay_ms(3);
-			pressure = readMPL115A2Pressure();
-			usbMsgPtr = (usbMsgPtr_t)(&pressure);
-			return sizeof(pressure);
+			#endif
+			/* altitude = */readMPLx115Altitude();
+			//usbMsgPtr = (usbMsgPtr_t)(&altitude);
+			//return sizeof(altitude);
+			usbMsgPtr = (usbMsgPtr_t)(dataBuf + 3);
+			return 3;
+
+		case USB_TEMPERATURE:
+			startMPLx115Read();
+			#if !defined(USING_MPL3115)
+			_delay_ms(3);
+			#endif
+			/* altitude = */readMPLx115Temperature();
+			usbMsgPtr = (usbMsgPtr_t)(dataBuf + 3);
+			return 3;
 
 /*
 		case USB_DATA_WRITE: // modify reply buffer
@@ -341,7 +570,7 @@ int main(void)
 	uint16_t PACIFIER_OFF_TGT = HEARTBEAT_TICK / DATA_TICK;
 	uint16_t PACIFIER_ON_TGT = PACIFIER_OFF_TGT - (HEARTBEAT_ON_TIME / DATA_TICK);
 	uint16_t PACIFIER_FULL_TGT = HEARTBEAT_FULL_TIME / DATA_TICK;
-	int16_t pressure;
+	int16_t altitude;
 	
 #define CHECK_WRITE(b) { if (!memoryFull) memoryFull = writeFlashByte(b); }
 
@@ -379,8 +608,10 @@ int main(void)
 	
 // Initialize all bus devices
 	deviceStatus = 0;
-	deviceStatus |= initMPL115A2() << DEV_GOOD_MPL115A2;
+	deviceStatus |= initMPLx115() << DEV_GOOD_MPLX115;
 	deviceStatus |= initFlash() << DEV_GOOD_FLASH;
+	deviceStatus |= initMPU6050() << DEV_GOOD_MPU6050;
+	dataBuf[0] = deviceStatus;
 
 // Initialize USB
 	// TODO: Check whether this is working as expected for start-up when not connected to USB
@@ -415,13 +646,13 @@ int main(void)
 			if (usbConnectedLast) PORTD &= ~_BV(PORTD6); // turn status LED off immediately if just removed
 			
 			// Read altimeter
-			startMPL115A2Read();
+			startMPLx115Read();
 			_delay_ms(3);  // maximum ADC conversion time
-			pressure = readMPL115A2Pressure();
+			altitude = readMPLx115Altitude();
 			
 			// Write data to flash
-			CHECK_WRITE(((uint16_t)pressure) >> 8);
-			CHECK_WRITE(((uint16_t)pressure) & 0xFF);
+			CHECK_WRITE(((uint16_t)altitude) >> 8);
+			CHECK_WRITE(((uint16_t)altitude) & 0xFF);
 
 			_delay_ms(DATA_TICK);  // should instead wait for a clock tick, not delay a specified amount!
 			
